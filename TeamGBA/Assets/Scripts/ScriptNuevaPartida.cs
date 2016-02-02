a52936c15b6b80e1d7cb717165ad4bf3d835a1d2
Venisir <sithis77@gmail.com>
2016-02-02 16:43:23 +0100

    Puestos botones en "Como jugar", "Creditos" y "Fin" que funcionan, ya no se cuelga y fondo gris, quitado el horizonte.

--- TeamGBA/Assets/Scripts/ScriptNuevaPartida.cs
+++ TeamGBA/Assets/Scripts/ScriptNuevaPartida.cs
@@ -4,27 +4,27 @@ using System.Collections;
 public class ScriptNuevaPartida : MonoBehaviour {
 	
 	public void nuevaPartida () {
-		GameObject.Find ("Button_NuevaPartida").GetComponent<AudioSource>().Play();
+		//GameObject.Find ("Button_NuevaPartida").GetComponent<AudioSource>().Play();
 		Invoke ("CargarNuevaPartida", 1);//GameObject.Find ("Button_NuevaPartida").GetComponent<AudioSource>().clip.length);
 	}	
 
 	public void volverAlMenu () {
-		GameObject.Find ("Button_VolverAlMenu").GetComponent<AudioSource>().Play();
+		//GameObject.Find ("Button_VolverAlMenu").GetComponent<AudioSource>().Play();
 		Invoke("CargarVolverAlMenu", 1);
 	}
 
 	public void creditos () {
-		GameObject.Find ("Button_Creditos").GetComponent<AudioSource>().Play();
+		//GameObject.Find ("Button_Creditos").GetComponent<AudioSource>().Play();
 		Invoke("CargarCreditos", 1);
 	}
 	
 	public void comoJugar () {
-		GameObject.Find ("Button_ComoJugar").GetComponent<AudioSource>().Play();
+		//GameObject.Find ("Button_ComoJugar").GetComponent<AudioSource>().Play();
 		Invoke("CargarComoJugar", 1);
 	}
 	
 	public void salir () {
-		GameObject.Find ("Button_Salir").GetComponent<AudioSource>().Play();
+		//GameObject.Find ("Button_Salir").GetComponent<AudioSource>().Play();
 		Invoke("CargarSalir", 1);
 	}
 	
@@ -33,7 +33,7 @@ public class ScriptNuevaPartida : MonoBehaviour {
 	}
 	
 	void CargarVolverAlMenu(){
-		Application.LoadLevel("MainMenu");
+		Application.LoadLevel("MainScene");
 	}
 	
 	void CargarCreditos(){
